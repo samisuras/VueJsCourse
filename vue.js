a89abcde13b8b5189bcd942ea20993d9a2bcf8f4
samisuras <raulsamuelleos98@gmail.com>
2019-09-18 15:49:16 -0500

    TODO list completado

--- vue.js
+++ vue.js
@@ -15,9 +15,19 @@ new Vue({
                 texto: 'Aprender React.js',
                 terminada: false
             },
-        ]
+        ],
+        nuevaTarea: ''
     },
     methods: {
-
+        agregarTarea: function() {
+            this.tareas.push({texto: this.nuevaTarea,terminada:false});
+            console.log(this.tareas)
+        },
+        eliminar: function(i){
+            this.tareas.splice(i,1);
+        },
+        cambiar: function(i){
+            this.tareas[i].terminada = !this.tareas[i].terminada;
+        }
     }
 })
