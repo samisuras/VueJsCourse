98a0e29a8a4e94c2c54fc8a0b4ac7b10c6d3ee6e
samisuras <raulsamuelleos98@gmail.com>
2019-09-18 16:15:29 -0500

    Todo en componentes

--- vue.js
+++ vue.js
@@ -41,9 +41,26 @@ Vue.component('nueva-tarea', {
     }
 })
 
-new Vue({
-    el: '#app',
-    data: data,
+Vue.component('lista-tareas', {
+    template: `<ul>
+            <template v-for="(tarea,i) in tareas">
+                <div class="card">
+                    <div class="card-body" >
+                        <span class="" v-bind:class="{terminada: tarea.terminada}">
+                            {{tarea.texto}}
+                        </span>
+                        
+                        <div class="derecha" style="display: inline;">
+                            <button class="btn btn-success" v-on:click="cambiar(i)" ><i class="fas fa-check"></i></button>
+                            <button class="btn btn-danger" v-on:click="eliminar(i)"> <i class="fas fa-times"></i> </button>
+                        </div>
+                    </div>
+                </div>
+            </template>
+        </ul>`,
+    data: function(){
+        return data;
+    },
     methods: {
         eliminar: function(i){
             this.tareas.splice(i,1);
@@ -53,3 +70,8 @@ new Vue({
         }
     }
 })
+
+new Vue({
+    el: '#app',
+    data: data
+});
