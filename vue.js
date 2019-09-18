5731507a10f729a4f4f449c6a403eb83902a44c6
samisuras <raulsamuelleos98@gmail.com>
2019-09-18 16:10:29 -0500

    Leccion Agregar tarea como componente

--- vue.js
+++ vue.js
@@ -1,3 +1,21 @@
+var data = {
+    tareas: [
+        {
+            texto: 'Aprender Vue.js',
+            terminada: false
+        },
+        {
+            texto: 'Aprender Angular.js',
+            terminada: false
+        },
+        {
+            texto: 'Aprender React.js',
+            terminada: false
+        },
+    ],
+    nuevaTarea: ''
+}
+
 Vue.component('titulo', {
     template: '<h1 align="center">{{titulo}}</h1>',
     data: function(){
@@ -5,31 +23,28 @@ Vue.component('titulo', {
     }
 })
 
-new Vue({
-    el: '#app',
-    data: {
-        
-        tareas: [
-            {
-                texto: 'Aprender Vue.js',
-                terminada: false
-            },
-            {
-                texto: 'Aprender Angular.js',
-                terminada: false
-            },
-            {
-                texto: 'Aprender React.js',
-                terminada: false
-            },
-        ],
-        nuevaTarea: ''
+Vue.component('nueva-tarea', {
+    template: `<div class="input-group mb-3">
+    <input type="text" class="form-control" v-on:keyup.enter="agregarTarea" v-model="nuevaTarea" placeholder="Hacer ejercicio">
+    <div class="input-group-append">
+        <span v-on:click="agregarTarea"  class="input-group-text btn btn-primary bg-primary text-light" id="basic-addon2">Agregar Tarea</span>
+        </div>
+    </div>`,
+    data: function(){
+        return data;
     },
     methods: {
         agregarTarea: function() {
             this.tareas.push({texto: this.nuevaTarea,terminada:false});
             console.log(this.tareas)
-        },
+        }
+    }
+})
+
+new Vue({
+    el: '#app',
+    data: data,
+    methods: {
         eliminar: function(i){
             this.tareas.splice(i,1);
         },
