53942e92f61aba78d42846285d5298deca4d5a47
samisuras <raulsamuelleos98@gmail.com>
2019-09-18 13:10:57 -0500

    Leccion 2

--- vue.js
+++ vue.js
@@ -1,36 +1,15 @@
-var app = new Vue({
-    el: '#vue',
+new Vue({
+    el: '#vm',
     data: {
-        mensaje: 'Aprende Vue.Js facilmente',
-        src: 'https://vuejs.org/images/logo.png'
+        primero: 0,
+        segundo: 0,
+        tercero: 0,
+        cuarto: 0,
+        suma: 0
     },
-    methods: {
-        mostrarMensaje: function(){
-            return this.mensaje;
+    computed: {
+        total: function(){
+            return this.primero + this.segundo + this.tercero + this.cuarto
         }
-    },
-    beforeCreate: function(){
-        console.log('LLamando beforeCreate');
-    },
-    created: function(){
-        console.log('LLmando create');
-    },
-    beforeMount: function(){
-        console.log('LLamando beforeMount');
-    },
-    mounted: function(){
-        console.log('LLamando mounted');
-    },
-    beforeUpdated: function(){
-        console.log('LLamando beforeUpdated');
-    },
-    updated: function(){
-        console.log('LLamando updated');
-    },
-    beforeDestroy: function(){
-        console.log('LLamando beforeDestroy');
-    },
-    destroy: function(){
-        console.log('LLamando destroy');
     }
-});
+})
