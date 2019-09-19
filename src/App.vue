331a6bade850ff6dbda98888927f358bae52f2b4
samisuras <raulsamuelleos98@gmail.com>
2019-09-19 16:58:41 -0500

    Directivas personalizadas

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <span>Aprende Vue.Js facilmente</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
