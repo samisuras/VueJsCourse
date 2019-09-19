33abfb10e30c3c99590f1ee1d1e63e2efc3c3df2
samisuras <raulsamuelleos98@gmail.com>
2019-09-19 17:16:48 -0500

    Leccion: Directivas personalizadas locales

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,30 @@
   <div class="container">
     <div class="jumbotron">
       <span v-decorar:grande.negrilla.italico="{familia: 'helvetica',color: 'red'}">Aprende Vue.Js facilmente</span>
+      <hr>
+      <span v-decorar-local:chico.negrilla="{familia: 'sans serif',color: 'blue'}">Aprende Angular.Js facilmente</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  directives: {
+    'decorar-local': {
+      bind(el,binding,vnode){
+        el.style.fontFamily = binding.value.familia;
+        el.style.color = binding.value.color;
+        if(binding.modifiers['negrilla'])
+          el.style.fontWeight = 'bold';
+        if(binding.modifiers['italico'])
+          el.style.fontStyle = 'Italic';
+        if(binding.arg == 'grande')
+          el.style.fontSize = '2rem'
+        if(binding.arg == 'chico')
+          el.style.fontSize = '.1rem'
+      }
+    }
+  },
   data () {
     return {}
   }
