e3514cf07b0dad8b8e3c3e1a9c69d85add56e11d
samisuras <raulsamuelleos98@gmail.com>
2019-09-19 16:24:13 -0500

    Leccion usando slots

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="container">
     <div class="jumbotron">
-      <mostrarFormulario/>
+      <mostrarFormulario>
+        <h3>Iniciar Sesion</h3>
+        <div class="form-group">
+            <label for="correo">Correo Electronico</label>
+            <input type="email" class="form-control" id="correo"
+            placeholder="Escribe tu correo">
+        </div>
+        <div class="form-group">
+            <label for="clave">Contrasena</label>
+            <input type="password" class="form-control" id="clave"
+            placeholder="Escribe tu contrasena">
+        </div>
+        <button class="btn btn-primary" >Iniciar Sesion</button>
+      </mostrarFormulario>
     </div>
   </div>
 </template>
--- src/mostrarFormulario.vue
+++ src/mostrarFormulario.vue
@@ -1,20 +1,12 @@
 <template>
     <form class="form">
-        <h3>Iniciar Sesion</h3>
-        <div class="form-group">
-            <label for="correo">Correo Electronico</label>
-            <input type="email" class="form-control" id="correo"
-            placeholder="Escribe tu correo">
-        </div>
-        <div class="form-group">
-            <label for="clave">Contrasena</label>
-            <input type="password" class="form-control" id="clave"
-            placeholder="Escribe tu contrasena">
-        </div>
-        <button class="btn btn-primary" >Iniciar Sesion</button>
+        <slot></slot>
     </form>
 </template>
 <script>
+export default{
+    
+}
 </script>
 <style>
 
