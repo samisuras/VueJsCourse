d3e228503a80352a3703699adee8972b6f0f0e0f
samisuras <raulsamuelleos98@gmail.com>
2019-09-19 15:06:29 -0500

    Lista de tareas creada

---
+++ src/listaTareas.vue
@@ -0,0 +1,43 @@
+<template>
+    <ul>
+        <template v-for="(tarea,i) in tareas">
+            <div class="card">
+                <div class="card-body" >
+                    <span class="" v-bind:class="{terminada: tarea.terminada}">
+                        {{tarea.texto}}
+                    </span>
+                    
+                    <div class="derecha" style="display: inline;">
+                        <button class="btn btn-success" v-on:click="cambiar(i)" ><i class="fas fa-check"></i></button>
+                        <button class="btn btn-danger" v-on:click="eliminar(i)"> <i class="fas fa-times"></i> </button>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['tareas'],
+    methods: {
+        eliminar: function(i){
+            this.tareas.splice(i,1);
+        },
+        cambiar: function(i){
+            this.tareas[i].terminada = !this.tareas[i].terminada;
+        }
+    }
+}
+</script>
+
+<style>
+.derecha{
+    align-items: right;
+    float: right;
+}
+.terminada{
+    color: grey;
+    text-decoration: line-through;
+}
+</style>
