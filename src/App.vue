c42ccc0290961113879592a5688f105b924e6043
samisuras <raulsamuelleos98@gmail.com>
2019-09-19 16:31:23 -0500

    Leccion: CSS en slots

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  h3{
+    color: red;
+  }
 </style>
--- src/mostrarFormulario.vue
+++ src/mostrarFormulario.vue
@@ -10,6 +10,8 @@ export default{
     
 }
 </script>
-<style>
-
+<style scoped>
+    h3{
+        color:blue;
+    }
 </style>
