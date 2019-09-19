e5881cc43ee9b978d144d7165ae0a27460d0ec11
samisuras <raulsamuelleos98@gmail.com>
2019-09-19 16:12:21 -0500

    Leccion slots Introduccion

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <mostrarFormulario/>
+    </div>
+  </div>
+</template>
+
+<script>
+import mostrarFormulario from './mostrarFormulario';
+
+export default {
+  components: {
+    mostrarFormulario
+  },
+  data () {
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/mostrarFormulario.vue
@@ -0,0 +1,21 @@
+<template>
+    <form class="form">
+        <h3>Iniciar Sesion</h3>
+        <div class="form-group">
+            <label for="correo">Correo Electronico</label>
+            <input type="email" class="form-control" id="correo"
+            placeholder="Escribe tu correo">
+        </div>
+        <div class="form-group">
+            <label for="clave">Contrasena</label>
+            <input type="password" class="form-control" id="clave"
+            placeholder="Escribe tu contrasena">
+        </div>
+        <button class="btn btn-primary" >Iniciar Sesion</button>
+    </form>
+</template>
+<script>
+</script>
+<style>
+
+</style>
