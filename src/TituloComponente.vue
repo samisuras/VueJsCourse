afe886422c6c4e8002e55ebf261b44bc256a2eee
samisuras <raulsamuelleos98@gmail.com>
2019-09-19 15:15:54 -0500

    Leccion validacion de propiedades

--- src/TituloComponente.vue
+++ src/TituloComponente.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="">
-        <h1>{{titulo}}</h1>
+        <h1>{{tituloMayusculas()}}</h1>
         <h3>Tareas a hacer</h3>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['titulo']
+    props: {
+        titulo: {
+            type: String,
+            required: true,
+            default: "tareas"
+        }
+    },
+    methods: {
+        tituloMayusculas(){
+            return this.titulo.toUpperCase();
+        }
+    }
 }
 </script>
